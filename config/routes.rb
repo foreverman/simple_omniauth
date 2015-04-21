9dc6e82b4f5a52bb1da0b0c440144c5756c653d1
foreverman <lzm@zhuopucapital.com>
2015-04-21 11:29:06 +0800

    added omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :articles
   root to: 'articles#index'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+  get 'auth/failure', to: redirect('/')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421020827) do
+ActiveRecord::Schema.define(version: 20150421031719) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20150421020827) do
     t.text     "content"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.integer  "uid"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
